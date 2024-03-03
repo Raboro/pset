c2f493247f248e286fee9d5c152cd2950dab9173
Raboro <marius.woerfelreal@gmail.com>
2024-03-03 23:11:21 +0100

    created template struct with render and display implementation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clap::Parser;
 
 mod args;
 
+mod template;
+
 fn main() {
     let args = args::Args::parse();
     println!("{}", args);
---
+++ src/template.rs
@@ -0,0 +1,43 @@
+use sailfish::TemplateOnce;
+
+pub struct Template<'a, T: TemplateOnce> {
+    filename: &'a str,
+    file_extension: &'a str,
+    file_path: Option<&'a str>,
+    #[allow(dead_code)]
+    template: T,
+}
+
+impl<'a, T: TemplateOnce> Template<'a, T> {
+    #[allow(dead_code)]
+    pub fn new(
+        filename: &'a str,
+        file_extension: &'a str,
+        file_path: Option<&'a str>,
+        template: T,
+    ) -> Self {
+        Self {
+            filename,
+            file_extension,
+            file_path,
+            template,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn render(self) -> Option<String> {
+        self.template.render_once().ok()
+    }
+}
+
+impl<'a, T: TemplateOnce> std::fmt::Display for Template<'a, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}.{}",
+            self.file_path.unwrap_or(""),
+            self.filename,
+            self.file_extension
+        )
+    }
+}
