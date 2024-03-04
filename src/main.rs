ef05f734cf589cf96fd9f96821a299b93c14a122
Raboro <marius.woerfelreal@gmail.com>
2024-03-04 21:34:14 +0100

    finished license template & test in main

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,15 @@ mod templates;
 
 fn main() {
     let args = args::Args::parse();
+    let license = templates::Template::new(
+        "license",
+        "md",
+        None,
+        templates::basics::License {
+            author: "Marius Wörfel",
+            year: 2024,
+        },
+    );
+    print!("{}", license.render().unwrap_or_default());
     println!("{}", args);
 }
--- src/templates/basics/mod.rs
+++ src/templates/basics/mod.rs
@@ -2,4 +2,7 @@ use sailfish::TemplateOnce;
 
 #[derive(TemplateOnce)]
 #[template(path = "basics/license.stpl")]
-pub struct License {}
+pub struct License<'a> {
+    pub author: &'a str,
+    pub year: u16,
+}
