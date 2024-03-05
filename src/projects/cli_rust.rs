8ff5a040af397ffc342418a417b4aeab40021bd1
Raboro <marius.woerfelreal@gmail.com>
2024-03-05 22:29:15 +0100

    generated cli rust with cargo new and base

--- src/projects/cli_rust.rs
+++ src/projects/cli_rust.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use super::{BaseProject, Project};
 
 pub struct CliRust {
@@ -5,5 +7,13 @@ pub struct CliRust {
 }
 
 impl Project for CliRust {
-    fn build(&self) {}
+    fn build(&self) {
+        Command::new("cargo")
+            .arg("new")
+            .arg(&self.base.name)
+            .output()
+            .expect("Cannot create cargo project");
+        self.base.create_license();
+        self.base.create_readme();
+    }
 }
