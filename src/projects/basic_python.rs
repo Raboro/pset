85dd438390a0901abefd2e109aafb6729b568bd2
Raboro <marius.woerfelreal@gmail.com>
2024-03-05 22:46:25 +0100

    added basic_python

--- src/projects/basic_python.rs
+++ src/projects/basic_python.rs
@@ -1,3 +1,8 @@
+use crate::{
+    fs,
+    templates::{python_main::PythonMain, Template},
+};
+
 use super::{BaseProject, Project};
 
 pub struct BasicPython {
@@ -5,5 +10,12 @@ pub struct BasicPython {
 }
 
 impl Project for BasicPython {
-    fn build(&self) {}
+    fn build(&self) {
+        self.base.build();
+        fs::create_dir(&format!("./{}/src", self.base.name)).expect("Cannot create src");
+
+        let main = Template::new("main", "py", Some("src"), &self.base.name, PythonMain {});
+        fs::create_file(main.to_path_buf(), main.render().unwrap_or_default())
+            .expect("Main cannot be created");
+    }
 }
---
+++ src/templates/python_main.rs
@@ -0,0 +1,5 @@
+use sailfish::TemplateOnce;
+
+#[derive(TemplateOnce)]
+#[template(path = "python_main.stpl")]
+pub struct PythonMain;
