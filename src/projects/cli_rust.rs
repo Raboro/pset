0be532f99ad71e255f807def8c8ec75a3b1a58f4
Raboro <marius.woerfelreal@gmail.com>
2024-03-06 23:38:44 +0100

    fixed clippy issues

--- src/projects/cli_rust.rs
+++ src/projects/cli_rust.rs
@@ -21,11 +21,10 @@ impl Project for CliRust {
         self.base.create_license();
         self.base.create_readme();
         */
-        let mut with: Vec<(String, String, Option<String>)> = vec![];
-        with.push(("filter".to_string(), "fetch".to_string(), None));
+        let with: Vec<(String, String, Option<String>)> =
+            vec![("filter".to_string(), "fetch".to_string(), None)];
 
-        let mut env: Vec<(String, String)> = vec![];
-        env.push(("gh_token".to_string(), "token".to_string()));
+        let env: Vec<(String, String)> = vec![("gh_token".to_string(), "token".to_string())];
         let ci_step = CiStep {
             name: "Test".to_string(),
             _if: Some("Hello there".to_string()),
@@ -35,11 +34,10 @@ impl Project for CliRust {
             env: Some(env),
         };
 
-        let mut jobs: Vec<Job> = vec![];
-        jobs.push(Job {
+        let jobs: Vec<Job> = vec![Job {
             name: "build".to_string(),
             steps: vec![ci_step],
-        });
+        }];
         let ci = Ci {
             workflow_name: "Build".to_string(),
             jobs,
