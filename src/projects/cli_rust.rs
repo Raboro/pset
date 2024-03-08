ae820f269d7b3b9ebd59b5b24a842d080dac5a90
Raboro <marius.woerfelreal@gmail.com>
2024-03-08 23:40:37 +0100

    completed cli-rust with ci

--- src/projects/cli_rust.rs
+++ src/projects/cli_rust.rs
@@ -1,9 +1,13 @@
-use sailfish::TemplateOnce;
+use std::process::Command;
 
-use crate::templates::{
-    ci::CiBuilder,
-    ci_job::{CiJob, CiJobBuilder},
-    ci_step::{CiStep, CiStepBuilder},
+use crate::{
+    fs,
+    templates::{
+        ci::{self, CiBuilder},
+        ci_job::CiJobBuilder,
+        ci_step::CiStepBuilder,
+        Template,
+    },
 };
 
 use super::{BaseProject, Project};
@@ -14,93 +18,69 @@ pub struct CliRust {
 
 impl Project for CliRust {
     fn build(&self) {
-        /*Command::new("cargo")
+        Command::new("cargo")
             .arg("new")
             .arg(&self.base.name)
             .output()
             .expect("Cannot create cargo project");
         self.base.create_license();
         self.base.create_readme();
-        */
-        let with: Vec<(String, String)> = vec![("filter".to_string(), "fetch".to_string())];
 
-        let env: Vec<(String, String)> = vec![("gh_token".to_string(), "token".to_string())];
-        let ci_step = CiStep {
-            name: "Test".to_string(),
-            _if: Some("Hello there".to_string()),
-            run: Some("Hello".to_string()),
-            uses: Some("action".to_string()),
-            with: Some(with),
-            env: Some(env),
-        };
+        fs::create_dir(&format!("./{}/.github", self.base.name))
+            .expect(".github folder cannot be generated");
+        fs::create_dir(&format!("./{}/.github/workflows", self.base.name))
+            .expect("workflows folder cannot be generated");
 
-        let with_long: Vec<(String, String)> = vec![
-            ("filter".to_string(), "fetch".to_string()),
-            ("filter".to_string(), "decode".to_string()),
-        ];
-
-        let env_long: Vec<(String, String)> = vec![
-            ("gh_token".to_string(), "token".to_string()),
-            ("sonar_token".to_string(), "sonar_token".to_string()),
-        ];
-
-        let ci_step_big = CiStep {
-            name: "Test".to_string(),
-            _if: Some("Hello there".to_string()),
-            run: Some("Hello".to_string()),
-            uses: Some("action".to_string()),
-            with: Some(with_long),
-            env: Some(env_long),
-        };
-
-        let jobs: Vec<CiJob> = vec![
-            CiJob {
-                name: "build".to_string(),
-                steps: vec![ci_step.clone()],
-            },
-            CiJob {
-                name: "test".to_string(),
-                steps: vec![ci_step.clone(), ci_step.clone()],
-            },
-            CiJob {
-                name: "verify".to_string(),
-                steps: vec![ci_step_big],
-            },
-        ];
-
-        let builder = CiStepBuilder::new()
-            .name("Test")
-            ._if("is null")
-            .uses("checkout")
-            .with(vec![("Test", "hello2"), ("Test2", "hello2")])
-            .env(vec![("Hi", "hii")]);
-        println!("{:#?}", builder);
-        println!("{}", builder.build().render_once().unwrap());
-
-        let builder2 = CiStepBuilder::new()
-            .name("Test")
-            ._if("is null")
-            .run("npm run dev");
-        println!("{:#?}", builder2);
-        println!("{}", builder2.build().render_once().unwrap());
-
-        let ci = CiBuilder::new()
-            .workflow_name("Build")
-            .init_jobs(jobs.first().unwrap().to_owned())
-            .add_job(jobs.get(2).unwrap().to_owned());
-
-        println!("\n\n{}", ci.build().render_once().unwrap());
-
-        let job = CiJobBuilder::new()
-            .name("Test")
-            .init_step(CiStepBuilder::new().name("test").run("npm test").build())
-            .add_step(
-                CiStepBuilder::new()
+        let ci: ci::Ci = CiBuilder::new()
+            .workflow_name("CI")
+            .init_jobs(
+                CiJobBuilder::new()
                     .name("build")
-                    .run("npm  run build")
+                    .init_step(
+                        CiStepBuilder::new()
+                            .name("Check")
+                            .run("cargo check")
+                            .build(),
+                    )
+                    .add_step(
+                        CiStepBuilder::new()
+                            .name("Clippy fix")
+                            .run("cargo clippy --fix")
+                            .build(),
+                    )
+                    .add_step(
+                        CiStepBuilder::new()
+                            .name("Check no more Clippy errors")
+                            .run("cargo clippy -- -D warnings")
+                            .build(),
+                    )
+                    .add_step(CiStepBuilder::new().name("Format").run("cargo fmt").build())
+                    .add_step(CiStepBuilder::new().name("Commit changes").run("|\n          git config user.name github-actions[bot]\n          git config user.email github-actions[bot]@users.noreply.github.com\n          git commit -am formatted and clippy || true").build())
+                    .add_step(
+                        CiStepBuilder::new()
+                            .name("Push changes")
+                            .uses("ad-m/github-push-action@master")
+                            .with(vec![
+                                ("github_token", "${{ secrets.GITHUB_TOKEN }}"),
+                                ("branch", "${{ github.ref }}"),
+                            ])
+                            .build(),
+                    )
+                    .add_step(
+                        CiStepBuilder::new()
+                            .name("Build")
+                            .run("cargo build")
+                            .build(),
+                    )
+                    .add_step(CiStepBuilder::new().name("Test").run("cargo test").build())
                     .build(),
-            );
+            )
+            .build();
 
-        println!("{:#?}", job.build());
+        let ci_template =
+            Template::new("ci", "yml", Some(".github/workflows"), &self.base.name, ci);
+
+        fs::create_file(ci_template.to_path_buf(), ci_template.render().unwrap())
+            .expect("Ci cannot be generated");
     }
 }
