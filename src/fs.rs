08bc380bfe6804149afec841d3e967a8b54c46c6
Raboro <marius.woerfelreal@gmail.com>
2024-03-09 11:05:55 +0100

    created function to create all nested dirs

--- src/fs.rs
+++ src/fs.rs
@@ -1,9 +1,19 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{Error, Write};
 use std::path::{Path, PathBuf};
 
 pub fn create_dir(path: &str) -> std::io::Result<()> {
     std::fs::create_dir(Path::new(path))
+}
+
+pub fn create_nested_dirs(full_path: &str, base: &str) {
+    let paths: Vec<&str> = full_path.split("/").collect();
+    let mut sub_path: String = format!("./{}", base);
+
+    for path in paths {
+        sub_path.push_str(&format!("/{}", path));
+        fs::create_dir(&sub_path).expect(&format!("Cannot create folder {}", sub_path));
+    }
 }
 
 pub fn create_file(path_buf: PathBuf, content: String) -> Result<(), Error> {
